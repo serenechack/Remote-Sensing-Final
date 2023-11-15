76e4c3f0fac62d66b8bd09f7ec9fafa5aaf616b0
poltergeistjoya <joyaranidebi@gmail.com>
2023-11-15 16:47:07 -0500

    Website grreaat

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Remote-Sensing-Final
+be# Remote-Sensing-Final
 
 # Here are somethings you may need to to do run this script
 # cd into remotesensing
 
 # npm install
 # npm install react-compare-slider
+# npm install react-slider
 # npm install --save react-card-flip
 # npm start
 
--- remotesensing/src/App.js
+++ remotesensing/src/App.js
@@ -2,18 +2,16 @@ import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slide
 import ReactCardFlip from 'react-card-flip'
 import React from 'react'
 import './App.css'
-// import gaza1 from "./gz1.png"
-// import gaza2 from "./gz2.png"
-// import ukraine1 from "./ukraine1.png"
-// import ukraine2 from "./ukraine2.png"
-import ukraine_s1 from "./ukraine_s1.png"
-import ukraine_s2 from "./ukraine_s2.png"
-import pr_before from "./pr_before.png"
-import pr_after from "./pr_after.png"
-import gaza_before from "./gaza_before.png"
-import gaza_after from "./gaza_after.png"
-import china_before from "./china_before.png"
-import china_after from "./china_after.png"
+import ukraine1 from "./ukraine1.png"
+import ukraine2 from "./ukraine2.png"
+import pr1 from "./PR1.png"
+import pr2 from "./PR2.png"
+import gaza1 from "./gaza1.png"
+import gaza2 from "./gaza2.png"
+import china1 from "./China1.png"
+import china2 from "./China2.png"
+import dummy from "./dummy588x450.png"
+import dummy2 from './dummy2.png'
 
 const CustomHandle = () => {
   return <div style={{
@@ -44,19 +42,24 @@ class InfoCard extends React.Component {
 
   render() {
     return (
-      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-        <div class = "flipcard-container">
-          <h1>
-            What is this conflict?
-          </h1>
-          <button className="btn" onClick={this.handleClick}>Click to flip</button>
+      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+        <div class = 'image' onClick={this.handleClick}>
+          <ReactCompareSlider
+            handle = {<CustomHandle />}
+            changePositionOnHover={true}
+            itemOne={this.props.itemOne}
+            itemTwo={this.props.itemTwo}
+          />
         </div>
-
-        <div class = "flipcard-container">
-          <h1>
+ 
+        <div class = "image" onClick={this.handleClick}>
+          <div>
           {this.props.text}
-          </h1>
-          <button className="btn" onClick={this.handleClick}>Click to flip</button>
+          </div>
+          <div>
+          {this.props.text2}
+          </div>
+       
         </div>
       </ReactCardFlip>
     )
@@ -66,90 +69,131 @@ class InfoCard extends React.Component {
 
 function App() {
   return (
-    <div>
-      <div class ="main-container">
-      <h1>Conflict Mapping With Night Time Light Data</h1>
-      <p> Hover over each image to see the difference</p>
+<div class="container">
+  <div class="headercontainer">
+    <div class ="header">Tracking Change with Night Time Lights </div>
+    <div class = "subheading">Hover over each image for night time light changes</div>
+    <div class = "subheading">Click for context</div>
+  </div>
+  <div class="Row">
+    <div class="green">
+      <div class = "image">
+        <InfoCard
+        text="Russian Invasion of Ukraine"
+        text2 = "February 2022"
+        itemOne={<ReactCompareSliderImage src={ukraine1} alt="Image one" />}
+        itemTwo={<ReactCompareSliderImage src={ukraine2} alt="Image two" />}
+        />
+      </div>
+    </div>
 
-      <div class="sub-container">
+    <div class="green">
+      <div class = "image">
+      <InfoCard
+        text="Hurricane Irma and Maria on Puerto Rico"
+        text2="August 2017"
+        itemOne={<ReactCompareSliderImage src={pr1} alt="Image one" />}
+        itemTwo={<ReactCompareSliderImage src={pr2} alt="Image two" />}
+        />
+      </div>
+    </div>
+  </div>
 
-        <div class = 'container' style={{marginRight: '200px', width: 550, height: 450 }}>
-          <ReactCompareSlider
-            handle = {<CustomHandle />}
-            changePositionOnHover={true}
-            itemOne={<ReactCompareSliderImage src={ukraine_s1} alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src={ukraine_s2} alt="Image two" />}
-          />
-          <div>
-          <InfoCard text="Russian invasion of Ukraine, February 2022" />
-          </div>
-        </div>
+  <div class="Row">
+    <div class="green">
+      <div class = "image">
+      <InfoCard
+        text="Israel Palestine Conflict"
+        text2="October 2023"
+        itemOne={<ReactCompareSliderImage src={gaza1} alt="Image one" />}
+        itemTwo={<ReactCompareSliderImage src={gaza2} alt="Image two" />}
+        />
+      </div>
+    </div>
 
-        <div class = 'container' style={{marginLeft: '200px', width: 550, height: 450 }}>
-          <ReactCompareSlider
-            handle = {<CustomHandle />}
-            changePositionOnHover={true}
-            itemOne={<ReactCompareSliderImage src={pr_before} alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src={pr_after} alt="Image two" />}
-          />
-          <div>
-          <InfoCard text="Hurricane Irma and Maria's devastation of Puerto Rico, August 2017" />
-          </div>
-        </div>
+    <div class="green">
+      <div class = "image">
+      <InfoCard
+        text="Growth of China"
+        text2="2012 - 2022"
+        itemOne={<ReactCompareSliderImage src={china1} alt="Image one" />}
+        itemTwo={<ReactCompareSliderImage src={china2} alt="Image two" />}
+        />
+      </div>
+    </div>
+  </div>
+</div>
+    // <div class = 'main-container'>
+    //   <div class ="Header">
+    //     <div class = "header">Conflict Mapping With Night Time Light Data</div>
+    //     <div class = 'subheading'>Hover over each image to see the difference</div>
 
-      </div>   
+    //   <div class="Row">
+    //       <div class = "image">
+    //         <ReactCompareSlider
+    //           handle = {<CustomHandle />}
+    //           changePositionOnHover={true}
+    //           itemOne={<ReactCompareSliderImage src={dummy2} alt="Image one" />}
+    //           itemTwo={<ReactCompareSliderImage src={dummy2} alt="Image two" />}
+    //         />
+    //         <div class = 'infocard'>
+    //           <InfoCard text="Russian invasion of Ukraine, February 2022" />
+    //         </div>
+    //       </div>
+            
 
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+    //       <div class = "image">
+    //       <ReactCompareSlider
+    //         handle = {<CustomHandle />}
+    //         changePositionOnHover={true}
+    //         itemOne={<ReactCompareSliderImage src={dummy} alt="Image one" />}
+    //         itemTwo={<ReactCompareSliderImage src={dummy} alt="Image two" />}
+    //       />
+    //       <div class = 'infocard'>
+    //         <InfoCard text="Hurricane Irma and Maria's devastation of Puerto Rico, August 2017" />
+    //         </div>
+    //         </div>
+    //         </div>
+           
+    
 
 
-      <div class="sub-container">
+    //   <div class="Row">
 
-        <div class = 'container' style={{marginRight: '200px', width: 550, height: 450 }}>
-          <ReactCompareSlider
-            handle = {<CustomHandle />}
-            changePositionOnHover={true}
-            itemOne={<ReactCompareSliderImage src={china_before} alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src={china_after}  alt="Image two" />}
-          />
-          <div>
-          <InfoCard text="Growth of China, 2012-2023" />
-          </div>
-        </div>
+    //     <div class = 'container'>
+    //       <ReactCompareSlider
+    //         handle = {<CustomHandle />}
+    //         changePositionOnHover={true}
+    //         itemOne={<ReactCompareSliderImage src={dummy} alt="Image one" />}
+    //         itemTwo={<ReactCompareSliderImage src={dummy}  alt="Image two" />}
+    //       />
+    //       <div>
+    //       <InfoCard text="Growth of China, 2012-2023" />
+    //       </div>
+    //     </div>
 
-        <div class = 'container' style={{marginLeft: '200px', width: 550, height: 450 }}>
+    //     <div class = 'container'>
           
-          <ReactCompareSlider
-            handle = {<CustomHandle />}
-            changePositionOnHover={true}
-            itemOne={<ReactCompareSliderImage src={gaza_before} alt="Image one" />}
-            itemTwo={<ReactCompareSliderImage src={gaza_after}  alt="Image two" />}
-          />
-          <div>
-          <InfoCard text="Israel and Palestine, October 2023" />
-          </div>
+    //       <ReactCompareSlider
+    //         handle = {<CustomHandle />}
+    //         changePositionOnHover={true}
+    //         itemOne={<ReactCompareSliderImage src={dummy} alt="Image one" />}
+    //         itemTwo={<ReactCompareSliderImage src={dummy}  alt="Image two" />}
+    //       />
+    //       <div>
+    //       <InfoCard text="Israel and Palestine, October 2023" />
+    //       </div>
           
-        </div>
+    //     </div>
 
-      </div> 
+    //   </div> 
 
       
       
-    </div>
+    // </div>
 
 
-    </div>
+    // </div>
 
   
   );
